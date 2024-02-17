f4812a5e3ddd49d897bbb8ac7e5231f778f57b19
Major <major.max.englishbull@gmail.com>
2024-02-17 19:27:32 +0100

    17-02

--- core/App.php
+++ core/App.php
@@ -1,10 +1,13 @@
 <?php
 
 namespace Cineflix\Core;
+
 class App {
 
     public static function load()
     {
+        require '..\vendor\autoload.php';
+        
         echo 'Class: ' . __CLASS__ . ' | Function: ' . __FUNCTION__;
     }
 }
