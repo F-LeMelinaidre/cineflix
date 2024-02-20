88099ab803b3dcbc956c67cba3a1ae851cd286a6
F_LeMelinaidre <152033638+Major-Max@users.noreply.github.com>
2024-02-20 09:09:18 +0100

    Update App.php

--- Core/App.php
+++ Core/App.php
@@ -16,14 +16,74 @@ class App
         // Création de l'ensemble des routes de l'application
         $router = new Router();
 
-        $router->get('/', [],'home.index');
-        $router->get('/Auth/signin', [], 'auth.signin');
-        $router->get('/Auth/signout', [], 'auth.signout');
-        $router->get('/movies', [], 'movies.index');
-        $router->get('/movies/:slug-:id', ['params' => ['slug' => '[a-z\-0-9]+', 'id' => '[0-9]+']], 'movies.show');
-        $router->get('/profil', [], 'profil.index');
-        $router->get('/Streams', [], 'streaming.index');
-        $router->get('/User', [], 'user.index');
+        $router->get('/',
+                         [
+                             'controller' => 'home',
+                             'action' => 'index'
+                         ],
+                         'home.index'
+                    );
+        
+        $router->get('/Auth/signin',
+                         [
+                             'controller' => 'Auth',
+                             'action' => 'signin'
+                         ],
+                         'auth.signin'
+                    );
+        
+        $router->get('/Auth/signout',
+                         [
+                             'controller' => 'Auth',
+                             'action' => 'signout'
+                         ],
+                         'auth.signout'
+                    );
+        
+        $router->get('/Movies',
+                         [
+                             'controller' => 'movies',
+                             'action' => 'index'
+                         ],
+                         'movies.index'
+                    );
+        
+        $router->get('/Movies/:slug-:id',
+                     [
+                             'controller' => 'movies',
+                             'action' => 'show',
+                             'params' => [
+                                             'slug' => '[a-z\-0-9]+',
+                                             'id' => '[0-9]+'
+                                         ]
+                         ],
+                        'movies.show'
+                    );
+        
+        $router->get('/Profil',
+                         [
+                             'controller' => 'profil',
+                             'action' => 'index'
+                         ],
+                         'profil.index'
+                    );
+        
+        $router->get('/Streams',
+                         [
+                             'controller' => 'streaming',
+                             'action' => 'index'
+                         ],
+                         'streaming.index'
+                    );
+        
+        $router->get('/User',
+                         [
+                             'controller' => 'user',
+                             'action' => 'index'
+                         ],
+                         'user.index'
+                    );
+        
         $router->run();
 
         echo '<p>Class: ' . __CLASS__ . ' | Function: ' . __FUNCTION__ . '</p>';
