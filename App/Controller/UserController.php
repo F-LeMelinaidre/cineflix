1476d865a9dedbf4f29d27358ea62f10dcea5212
F_LeMelinaidre <152033638+Major-Max@users.noreply.github.com>
2024-02-20 09:41:05 +0100

    Update UserController.php

--- App/Controller/UserController.php
+++ App/Controller/UserController.php
@@ -7,21 +7,25 @@ class UserController
 
     public function __construct()
     {
+        echo '<br>Class: '.__CLASS__.'<br>';
 
     }
 
     public function index()
     {
+        echo 'Function: '.__FUNCTION__.'<br>Line: '.__LINE__.'<br><br>';
 
     }
 
     public function show()
     {
+        echo 'Function: '.__FUNCTION__.'<br>Line: '.__LINE__.'<br><br>';
 
     }
 
     public function edit()
     {
+        echo 'Function: '.__FUNCTION__.'<br>Line: '.__LINE__.'<br><br>';
 
     }
 
