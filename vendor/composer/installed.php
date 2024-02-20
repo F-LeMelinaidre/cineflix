bcd2e2cfe12527d6ed09e9e60885ca02867a8f0a
Major <major.max.englishbull@gmail.com>
2024-02-20 19:10:48 +0100

    20-02

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'projet-web/cineflix',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '37de49b0671eb60e0a4d5bc36216bfd20156e938',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'projet-web/cineflix' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '37de49b0671eb60e0a4d5bc36216bfd20156e938',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
