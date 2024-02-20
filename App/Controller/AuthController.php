e5825b150c797efd28962fb08fa24f7eaed4110c
F_LeMelinaidre <152033638+Major-Max@users.noreply.github.com>
2024-02-20 09:37:09 +0100

    Update AuthController.php

--- App/Controller/AuthController.php
+++ App/Controller/AuthController.php
@@ -7,21 +7,24 @@ class AuthController
 
     public function __construct()
     {
-
+        echo 'Class: '.__CLASS__.'<br>';
+        
     }
 
     public function signin()
     {
-
+        echo 'Function: '.__FUNCTION__.'<br>Line: '.__LINE__.'<br><br>';
     }
 
     public function signout()
     {
+        echo 'Function: '.__FUNCTION__.'<br>Line: '.__LINE__.'<br><br>';
 
     }
 
     public function logout()
     {
+        echo 'Function: '.__FUNCTION__.'<br>Line: '.__LINE__.'<br><br>';
 
     }
 }
