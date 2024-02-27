0a62e0f87362bbf89815229e3f7295fafdb77d8c
Major <major.max.englishbull@gmail.com>
2024-02-27 23:00:53 +0100

    class Database
    testé la connexion se fait
    et la recuperation de donnée aussi.
    Y a plus qu'à l'améliorer

--- Core/Database/Database.php
+++ Core/Database/Database.php
@@ -34,37 +34,37 @@ class Database
         return self::$_Instance;
     }
 
-    private function getPDO($dsn, $db_user, $db_pwd)
+    private function getPDO(string $dsn, string $db_user, string $db_pwd): void
     {
         try {
 
             $this->connexion = new PDO($dsn, $db_user, $db_pwd, $this->options);
         } catch (Exception $e) {
-            MessageFlash::create("Exception capturée: Connexion au SGBDR impossible! <br>" . $e->getMessage(), 'erreur');
-            header('Location: erreur/500');
+            echo 'erreur';
+            //MessageFlash::create("Exception capturée: Connexion au SGBDR impossible! <br>" . $e->getMessage(), 'erreur');
+            //header('Location: erreur/500');
         }
     }
 
-    public function prepare($query, $bindvalues)
+    public function prepare(string $query, array $bindvalues = [])
     {
         try {
-
+echo 'connexion';
             $pre = $this->connexion->prepare($query);
 
-            if (!is_null($bindvalues)) {
-
+            if (!empty($bindvalues)) {
+                var_dump($bindvalues);
                 foreach ($bindvalues as $val) {
                     $pre->bindValue($val['col'], $val['val'], PDO::PARAM_STR);
                 }
             }
 
             $pre->execute();
-
             return $pre;
         } catch (Exception $e) {
-
-            MessageFlash::create("Impossible de récupérer les données sur la table! <br>" . $e->getMessage(), 'erreur');
-            return false;
+            echo 'erreur';
+            //MessageFlash::create("Impossible de récupérer les données sur la table! <br>" . $e->getMessage(), 'erreur');
+            //return false;
         }
     }
 }
