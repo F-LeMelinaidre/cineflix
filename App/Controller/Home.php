fb5a52822aa755573c0da208152d4a467f8fbfc4
Major <major.max.englishbull@gmail.com>
2024-03-23 19:31:04 +0100

    up controller home  methode index vue index css

--- App/Controller/Home.php
+++ App/Controller/Home.php
@@ -2,8 +2,7 @@
 
 namespace Cineflix\App\Controller;
 
-use Cineflix\App\AppController;
-use Cineflix\App\Model\MovieModel;
+use Cineflix\App\Model\DAO\MovieDao;
 use Cineflix\Core\AbstractController;
 
 class Home extends AbstractController
@@ -11,15 +10,8 @@ class Home extends AbstractController
 
     public function index(): string
     {
-        $db = AppController::$_Database;
-        $query = "SELECT d.id AS id, d.nom AS nom, d.affiche AS affiche, d.slug AS slug, c.nom AS cinema, v.nom AS ville 
-                    FROM film AS f
-                    JOIN fiche d ON f.fiche_id = d.id
-                    JOIN cinema c ON f.cinema_id = c.id
-                    JOIN ville v ON c.ville_id = v.id ";
-
-        $req = $db->prepare($query);
-        $movies= $req->fetchAll(MovieModel::class);
+        $movieDao = new MovieDao();
+        $movies = $movieDao->findAll();
 
         return $this->render('Home.index',compact('movies'));
     }
