862b66dcb06621a8b8eb72960be093056929e1d3
F_LeMelinaidre <152033638+Major-Max@users.noreply.github.com>
2024-03-27 18:08:44 +0100

    Update MovieModel.php

--- App/model/MovieModel.php
+++ App/model/MovieModel.php
@@ -5,17 +5,15 @@ namespace Cineflix\App\Model;
 class MovieModel extends FicheModel
 {
 
-    private int $fiche_id;
-    private int $cinema_id;
-    private int $exploitation_id;
+    public $movie_id;
+    public $cinema;
+    public $ville;
+    public int $fiche_id;
+    public int $cinema_id;
+    public int $exploitation_id;
     public string $exploitation_debut = '';
     public string $exploitation_fin = '';
 
-    public function __construct(int $fiche_id, string $nom, string $synopsis, string $date_sortie, string $affiche, string $slug, int $movie_id,  $cinema, $ville)
-    {
-        parent::__construct($fiche_id, $nom, $synopsis, $date_sortie, $affiche, $slug);
-        $this->nom = $nom;
 
-    }
 
 }
