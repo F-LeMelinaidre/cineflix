2b552f655449c28efc5ff2df4e5131d14fe4ecc4
Major <major.max.englishbull@gmail.com>
2024-03-27 07:42:27 +0100

    new

--- App/View/Movie/Admin/edit.php
+++ App/View/Movie/Admin/edit.php
@@ -8,29 +8,29 @@
         <h2 class="titre">Information du film</h2>
   
         <label class="form-label" for="InputNom">Nom</label>
-        <input id="InputNom" class="form-control" type="text" name="nom" value="<?= $movie->nom ?>">
+        <input id="InputNom" class="form-control" type="text" name="nom" value="<?= $movie->nom ?>" aria-required="true">
         <div id="MoviesList"></div>
 
         <label class="form-label" for="InputDateSortie">Date de sortie</label>
-        <input id="InputDateSortie" class="form-control" type="date" name="date_sortie" value="<?= $movie->date_sortie ?>">
+        <input id="InputDateSortie" class="form-control" type="date" name="date_sortie" value="<?= $movie->date_sortie ?>" aria-required="true">
 
         <label class="form-label" for="InputCinema">Cinema</label>
         <input id="InputCinema" class="form-select" type="text" name="cinema" value="<?= $movie->cinema. ' ' .$movie->ville ?? '' ?>"
-               placeholder="Saisissez un cinéma ou une ville">
+               placeholder="Saisissez un cinéma ou une ville" aria-required="true">
         <div id="CinemasList"></div>
         <input type="hidden" id="cinema_id" name="cinema_id" value="">
 
         <h2 class="exploitation">Information d'exploitation</h2>
         <label class="form-label" for="InputDateDebut">Début</label>
-        <input id="InputDateDebut" class="form-control" type="date" name="exploitation_debut" value="<?= $movie->exploitation_debut ?>">
+        <input id="InputDateDebut" class="form-control" type="date" name="exploitation_debut" value="<?= $movie->exploitation_debut ?>" aria-required="true">
         <label class="form-label" for="InputDateFin">Fin</label>
-        <input id="InputDateFin" class="form-control" type="date" name="exploitation_fin" value="<?= $movie->exploitation_fin ?>">
+        <input id="InputDateFin" class="form-control" type="date" name="exploitation_fin" value="<?= $movie->exploitation_fin ?>" aria-required="true">
 
         <label class="form-label" for="TextareaSynopsis">Synopsis</label>
-        <textarea id="TextareaSynopsis" class="form-control" name="synopsis" cols="30" rows="5"><?= $movie->synopsis ?></textarea>
+        <textarea id="TextareaSynopsis" class="form-control" name="synopsis" cols="30" rows="5" aria-required="true"><?= $movie->synopsis ?></textarea>
 
         <label class="form-label" for="InputAffiche">Affiche</label>
-        <input id="InputAffiche" class="form-control" name="affiche" type="file" accept=".png, .jpg, .jpeg">
+        <input id="InputAffiche" class="form-control" name="affiche" type="file" accept=".png, .jpg, .jpeg" aria-required="true">
         <div class="thumb"><img src="" alt="" width="120px"></div>
 
         <input type="hidden" id="fiche_id" name="fiche_id" value="">
--- public/js/validationLib.js
+++ public/js/validationLib.js
@@ -32,9 +32,14 @@ const rules = {
 
 document.addEventListener('DOMContentLoaded', function () {
 
-    const items = document.querySelectorAll('input');
+    const items = document.querySelectorAll('input, textarea');
+
+    let invalidInputs = [];
+
     let position = 0;
     items.forEach(function (item, i) {
+
+
 
 
 
