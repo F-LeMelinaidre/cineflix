b3329482033685ad0d708d2d79f7e2767001cbbc
F_LeMelinaidre <152033638+Major-Max@users.noreply.github.com>
2024-03-28 00:43:01 +0100

    Update AccountModel.php

--- App/model/AccountModel.php
+++ App/model/AccountModel.php
@@ -6,19 +6,9 @@
 
     class AccountModel
     {
-
-        public string $nom = '';
-        public string $prenom;
-        public string $mail;
-        public string $password;
-
-        public array $errors;
-
         public function __construct()
         {
 
         }
-
-
 
     }
