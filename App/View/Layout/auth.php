d59be341946f33b942d58d97fc9df1ceaede7372
Major <major.max.englishbull@gmail.com>
2024-03-29 19:52:18 +0100

    up

--- App/View/Layout/auth.php
+++ App/View/Layout/auth.php
@@ -1,5 +1,5 @@
 
-<main class="container-xl container-fluid mx-lg-auto my-5">
+<main class="container-xl container-fluid mx-lg-auto p-0 py-5">
     {{content}}
 </main>
 <script src="/public/js/jquery-3.7.1.min.js"></script>
--- App/model/DAO/MovieDao.php
+++ App/model/DAO/MovieDao.php
@@ -42,11 +42,11 @@ class MovieDao
                 $clause = 'id LIKE :id';
         }
 
-        $query = "SELECT f.id AS id, d.nom AS nom, d.synopsis AS synopsis,
-                    d.affiche AS affiche, d.date_sortie AS date_sortie, d.slug AS slug, c.nom AS cinema,
-                    v.nom AS ville
-                    FROM film AS f JOIN fiche d ON f.fiche_id = d.id JOIN cinema c ON f.cinema_id = c.id JOIN ville v ON c.ville_id = v.id
-                    WHERE d.$clause";
+        $query = "SELECT film.id AS movie_id, fiche.id AS detail_id, fiche.nom AS nom, fiche.synopsis AS synopsis,
+                  fiche.affiche AS affiche, fiche.date_sortie AS date_sortie, fiche.slug AS slug, cinema.nom AS cinema,
+                  ville.nom AS ville
+                  FROM film AS film JOIN fiche ON film.fiche_id = fiche.id JOIN cinema ON film.cinema_id = cinema.id JOIN ville ON cinema.ville_id = ville.id
+                  WHERE fiche.$clause";
 
         $binvalue[] = ['col' => $item, 'val' => $value];
         $req = $this->db->prepare($query, $binvalue);
