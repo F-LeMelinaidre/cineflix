b9b5bcdb7ae2f9b345982d1f1d23cb1643979002
Major <major.max.englishbull@gmail.com>
2024-04-15 22:21:38 +0200

    up

--- App/View/Home/index.php
+++ App/View/Home/index.php
@@ -7,7 +7,7 @@
     <h1 class="flex-grow-1 ms-5 mb-0">Films en salles</h1>
 </header>
 <section class="container d-flex justify-content-center row-gap-4 column-gap-4 flex-wrap mt-4">
-    <nav class="col-12 d-flex ms-5 align-items-center">
+    <nav class="container-fluid d-flex ms-4 mb-4 align-items-center">
         <input type="text" class="form-control w-25 m-0">
         <a href="#" class="btn btn-sm btn-outline-warning ms-4">Rechercher</a>
     </nav>
