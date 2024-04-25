02e9e2d179653a7aa2f347b9698e127b94360c7f
Major <major.max.englishbull@gmail.com>
2024-04-26 01:07:52 +0200

    DataBase

--- App/model/AbstractModel.php
+++ App/model/AbstractModel.php
@@ -1,94 +1,108 @@
 <?php
 
-namespace Cineflix\App\model;
+    namespace Cineflix\App\model;
 
-use Cineflix\Core\Util\Regex;
+    use Cineflix\Core\Util\Regex;
 
-class AbstractModel
-{
-
-
-    private array $validation_items;
-    private array $errors = [];
-
-    protected ?int $id = null;
-    public string $created;
-    public string $modified;
-
-    public ?string $nom = null;
-
-    public function __construct(?array $data)
+    class AbstractModel
     {
-        if(isset($data['id'])) $this->id = $data['id'];
-        if(isset($data['created'])) $this->created = $data['created'];
-        if(isset($data['modified'])) $this->modified = $data['modified'];
-        if(isset($data['nom'])) $this->nom = $data['nom'];
 
-    }
-    public function getId(): int
-    {
-        return $this->id;
-    }
 
-    public function setId(int $id): void
-    {
-        $this->id = $id;
-    }
-    public function getDateFr(string $date): string
-    {
-        return date("d-m-Y H:i:s", strtotime($date));
-    }
+        private array $validation_items;
 
-    /**
-     * @param string $item
-     * @param array  $options
-     *
-     * @return void
-     */
-    public function addValidation(string $item, array $options): void
-    {
-        $this->validation_items[$item] = $options;
-    }
+        protected array $errors = [];
+        protected ?int $id = null;
 
-    /**
-     * @return bool
-     */
-    public function isValid(): bool
-    {
-        foreach($this->validation_items as $item => $rule) {
-            $this->validate($rule, $item);
+        public string $created;
+        public string $modified;
+
+        public ?string $nom = null;
+
+        public function __construct(?array $data)
+        {
+            if(isset($data['id'])) $this->id = $data['id'];
+            if(isset($data['created'])) $this->created = $data['created'];
+            if(isset($data['modified'])) $this->modified = $data['modified'];
+            if(isset($data['nom'])) $this->nom = $data['nom'];
+
+        }
+        public function getId(): int
+        {
+            return $this->id;
         }
 
-        return empty($this->errors);
-    }
+        public function setId(int $id): void
+        {
+            $this->id = $id;
+        }
+        public function getDateFr(string $date): string
+        {
+            return date("d-m-Y H:i:s", strtotime($date));
+        }
 
-    /**
-     * @param $rule
-     * @param $item
-     *
-     * @return void
-     */
-    private function validate($rule, $item)
-    {
-        $require = in_array('require', $rule);
-        $message = (isset($rule['message'])) ? $rule['message'] : null;
-        $rule = (isset($rule['rule'])) ? $rule['rule'] : null;
+        /**
+         * @param string $item
+         * @param array  $options
+         *
+         * @return void
+         */
+        public function addValidation(string $item, array $options): void
+        {
+            $this->validation_items[$item] = $options;
+        }
 
-        if($require && empty($this->$item)) {
+        /**
+         * @return bool
+         */
+        public function isValid(): bool
+        {
+            foreach($this->validation_items as $item => $rule) {
+                $this->validate($rule, $item);
+            }
 
-            $this->errors[$item] = (isset($message['require']))? $message['require'] : "Champ requis !";
+            return empty($this->errors);
+        }
 
-        } elseif(!empty($this->$item) && !is_null($rule) && !preg_match(Regex::getPattern($rule), $this->$item)) {
-            $this->errors[$item] = (isset($message[$rule]))? $message[$rule] : Regex::getMessage($rule);
+        /**
+         * @param $rule
+         * @param $item
+         *
+         * @return void
+         */
+        private function validate($rule, $item)
+        {
+            $require = in_array('require', $rule);
+            $message = (isset($rule['message'])) ? $rule['message'] : null;
+            $rule = (isset($rule['rule'])) ? $rule['rule'] : null;
+
+            if($require && empty($this->$item)) {
+
+                $this->errors[$item] = (isset($message['require']))? $message['require'] : "Champ requis !";
+
+            } elseif(!empty($this->$item) && !is_null($rule) && !preg_match(Regex::getPattern($rule), $this->$item)) {
+                $this->errors[$item] = (isset($message[$rule]))? $message[$rule] : Regex::getMessage($rule);
+            }
+        }
+
+        /**
+         * @return array
+         */
+        public function getErrors(): array
+        {
+            return $this->errors;
+        }
+
+        /**
+         * @param array|null $data
+         *
+         * @return void
+         */
+        public function hydrate(?array $data)
+        {
+            if(isset($data['id'])) $this->id = $data['id'];
+            if(isset($data['created'])) $this->created = $data['created'];
+            if(isset($data['modified'])) $this->modified = $data['modified'];
+            if(isset($data['nom'])) $this->nom = $data['nom'];
+
         }
     }
-
-    /**
-     * @return array
-     */
-    public function getErrors(): array
-    {
-        return $this->errors;
-    }
-
-}
